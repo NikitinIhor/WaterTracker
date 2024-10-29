b40091a91c1eaf4398e93673f0d117cebff86473
Nikitin <nikitinigor0907@gmail.com>
2024-10-29 15:17:51 +0100

    update

--- src/components/HomePage/Statistic/Statistic.jsx
+++ src/components/HomePage/Statistic/Statistic.jsx
@@ -1,14 +1,76 @@
-import { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-// import css from './Statistic.module.css'
+import water_icon from "../../../images/icons/gluss.png";
+import sprites from "../../../images/sprite.svg";
+import css from "./Statistic.module.css";
 
 export default function Statistic() {
-  const [value, onChange] = useState(new Date());
-
   return (
-    <div>
-      <Calendar onChange={onChange} value={value} />
+    <div className={css.wrapper}>
+      <div className={css.today}>
+        <h2>Today</h2>
+        <ul className={css.list}>
+          <li className={css.item}>
+            <div className={css.water}>
+              <img src={water_icon} alt="water_icon" />
+              <span>200 ml</span>
+            </div>
+            <div className={css.time}>14:00 PM</div>
+            <div className={css.btns}>
+              <svg className={css.edit}>
+                <use href={`${sprites}#icon-pen`}></use>
+              </svg>
+              <svg className={css.delete}>
+                <use href={`${sprites}#icon-delete`}></use>
+              </svg>
+            </div>
+          </li>
+          <li className={css.item}>
+            <div className={css.water}>
+              <img src={water_icon} alt="water_icon" />
+              <span>200 ml</span>
+            </div>
+            <div className={css.time}>14:00 PM</div>
+            <div className={css.btns}>
+              <svg className={css.edit}>
+                <use href={`${sprites}#icon-pen`}></use>
+              </svg>
+              <svg className={css.delete}>
+                <use href={`${sprites}#icon-delete`}></use>
+              </svg>
+            </div>
+          </li>
+          <li className={css.item}>
+            <div className={css.water}>
+              <img src={water_icon} alt="water_icon" />
+              <span>200 ml</span>
+            </div>
+            <div className={css.time}>14:00 PM</div>
+            <div className={css.btns}>
+              <svg className={css.edit}>
+                <use href={`${sprites}#icon-pen`}></use>
+              </svg>
+              <svg className={css.delete}>
+                <use href={`${sprites}#icon-delete`}></use>
+              </svg>
+            </div>
+          </li>
+          <li className={css.item}>
+            <div className={css.water}>
+              <img src={water_icon} alt="water_icon" />
+              <span>200 ml</span>
+            </div>
+            <div className={css.time}>14:00 PM</div>
+            <div className={css.btns}>
+              <svg className={css.edit}>
+                <use href={`${sprites}#icon-pen`}></use>
+              </svg>
+              <svg className={css.delete}>
+                <use href={`${sprites}#icon-delete`}></use>
+              </svg>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className={css.month}></div>
     </div>
   );
 }
